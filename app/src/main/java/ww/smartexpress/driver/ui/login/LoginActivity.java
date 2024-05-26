d4557af49402b9405af9a1a56d3d05920bc34257
SWIFT 3 <moonwolfjoker@gmail.com>
2024-05-26 10:27:30 +0700

    login gg

--- app/src/main/java/ww/smartexpress/driver/ui/login/LoginActivity.java
+++ app/src/main/java/ww/smartexpress/driver/ui/login/LoginActivity.java
@@ -1,19 +1,40 @@
 package ww.smartexpress.driver.ui.login;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.Observable;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import ww.smartexpress.driver.BR;
 import ww.smartexpress.driver.R;
 import ww.smartexpress.driver.databinding.ActivityLoginBinding;
 import ww.smartexpress.driver.di.component.ActivityComponent;
 import ww.smartexpress.driver.ui.base.activity.BaseActivity;
+import ww.smartexpress.driver.ui.main.MainActivity;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewModel> {
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+
+    private static final int REQ_ONE_TAP = 1000;
     @Override
     public int getLayoutId() {
         return R.layout.activity_login;
@@ -33,6 +54,44 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewM
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        oneTapClient = Identity.getSignInClient(this);
+
+        signInRequest = new BeginSignInRequest.Builder()
+                .setPasswordRequestOptions(new BeginSignInRequest.PasswordRequestOptions.Builder()
+                        .setSupported(true)
+                        .build())
+                .setGoogleIdTokenRequestOptions(new BeginSignInRequest.GoogleIdTokenRequestOptions.Builder()
+                        .setSupported(true)
+                        .setServerClientId("586602267884-ti3mmdqtmumjrvekreaj4tk96aqr5st2.apps.googleusercontent.com") // Web client ID từ Google Cloud Console
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                .setAutoSelectEnabled(true)
+                .build();
+
+        viewBinding.signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                oneTapClient.beginSignIn(signInRequest)
+                        .addOnSuccessListener(LoginActivity.this, new OnSuccessListener<BeginSignInResult>() {
+                            @Override
+                            public void onSuccess(BeginSignInResult result) {
+                                try {
+                                    startIntentSenderForResult(result.getPendingIntent().getIntentSender(), REQ_ONE_TAP, null, 0, 0, 0);
+                                } catch (IntentSender.SendIntentException e) {
+                                    Log.e("OneTap", "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+                                }
+                            }
+                        })
+                        .addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("OneTap", "Failed to start One Tap sign-in", e);
+                                Toast.makeText(getApplicationContext(), "Unable to start One Tap sign-in. Please try again later.", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }});
+
+
         viewModel.isVisibility.addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
             @Override
             public void onPropertyChanged(Observable sender, int propertyId) {
@@ -44,4 +103,22 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewM
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_ONE_TAP) {
+            try {
+                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                String idToken = credential.getGoogleIdToken();
+                if (idToken != null) {
+                    Log.d("OneTap", "Got ID token." + idToken);
+                    // Xử lý đăng nhập thành công tại đây
+                }
+            } catch (ApiException e) {
+                Log.e("OneTap", "Sign-in failed.", e);
+            }
+        }
+    }
+
 }
