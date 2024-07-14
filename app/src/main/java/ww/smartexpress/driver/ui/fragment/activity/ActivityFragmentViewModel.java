1a45393d38aba2498b9c569cff0840f6c7a0cdd7
SWIFT 3 <moonwolfjoker@gmail.com>
2024-07-14 11:29:50 +0700

    update ui

--- app/src/main/java/ww/smartexpress/driver/ui/fragment/activity/ActivityFragmentViewModel.java
+++ app/src/main/java/ww/smartexpress/driver/ui/fragment/activity/ActivityFragmentViewModel.java
@@ -79,6 +79,7 @@ public class ActivityFragmentViewModel extends BaseFragmentViewModel {
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(response -> {
                             if(response.isResult()){
+                                showSuccessMessage(response.getMessage());
                             }else {
                                 showErrorMessage(response.getMessage());
                             }
